79dbca50c855209aead2f408e62a38bb48e45f78
Pedro Fonseca <60857409+PedroFonsecaDEV@users.noreply.github.com>
2020-10-30 19:51:55 -0400

    FIXES #1223       *ScrollTop and ScrollDown becomes: ScrollAction (We had repeated code for up and down, now we have only one component)       *BackToTop becomes: BackToTopButton - No more coupling with ScrollTop       *Merge our two "sticky headers" on scroll (#1223)

--- src/frontend/src/components/Posts/Posts.jsx
+++ src/frontend/src/components/Posts/Posts.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useSWRInfinite } from 'swr';
-
 import { Container } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Timeline from './Timeline.jsx';
